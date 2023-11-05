7c9717358e6186638b6bfbde337dd4a717f8afde
MaiNgocGiau <2154050076giau@ou.edu.vn>
2023-11-05 11:30:11 +0700

    Kiem tra dieu kien

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -102,6 +102,7 @@ namespace Calculator
             this.btTru.TabIndex = 2;
             this.btTru.Text = "Trừ";
             this.btTru.UseVisualStyleBackColor = true;
+            this.btTru.Click += new System.EventHandler(this.btTru_Click);
             // 
             // Form1
             // 
